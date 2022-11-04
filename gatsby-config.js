a5805b4dc4d5fcd434d10475bc507f571b513309
Emanuel Juarez <emanueljuarez.webdev@gmail.com>
2022-11-04 15:16:42 -0600

    creacion de archivos base y dependencias

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,15 +3,44 @@ module.exports = {
     title: `tycoon`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "23223"
-    }
-  }, "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }]
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/locales/`,
+        name: `locale`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-react-i18next`,
+      options: {
+        localeJsonSourceName: `locale`, // name given to `gatsby-source-filesystem` plugin.
+        languages: [`es`, `en`],
+        defaultLanguage: `es`,
+        // if you are using Helmet, you must include siteUrl, and make sure you add http:https
+        siteUrl: `https://www.yourdomain.tld`,
+        // if you are using trailingSlash gatsby config include it here, as well (the default is 'always')
+        trailingSlash: "always",
+        // you can pass any i18next options
+        i18nextOptions: {
+          interpolation: {
+            escapeValue: false, // not needed for react as it escapes by default
+          },
+          keySeparator: false,
+          nsSeparator: false,
+        },
+        pages: [
+          {
+            matchPath: "/:lang?/blog/:uid",
+            getLanguageFromPath: true,
+            excludeLanguages: ["es"],
+          },
+          {
+            matchPath: "/preview",
+            languages: ["en"],
+          },
+        ],
+      },
+    },
+  ],
 };
---
+++ src/components/layout/header.jsx
@@ -0,0 +1,254 @@
+import React, { useState } from "react";
+import { Link } from "gatsby";
+import useWindowSize from "../../hooks/useWindowSize";
+import styled from "styled-components";
+import { useI18next, useTranslation } from "gatsby-plugin-react-i18next";
+import SwitchLeng from "./SwitchLeng";
+import Logo from "../../images/mobile/layout/header/logo-header.png"
+import Menu from "../../images/mobile/layout/header/menú.png"
+
+const HeaderTag = styled.header`
+  display: flex;
+  justify-content: space-between;
+  background-color: white;
+  align-items: center;
+  position: fixed;
+  width: 100%;
+  z-index: 888;
+
+  nav {
+    display: flex;
+    justify-content: space-between;
+    width: 40%;
+    padding: 3% 10% 2% 10%;
+  }
+  nav a {
+    font-family: "M Regular";
+    color: black;
+    text-decoration: none;
+    font-size: 20px;
+  }
+  a:hover {
+    color: #77d0d1 !important;
+    text-decoration: underline !important;
+    font-family: "M SemiBold";
+  }
+  > div:nth-child(1) img {
+    width: 40%;
+  }
+  > div:nth-child(1) {
+    padding: 1% 1% 1% 5%;
+  }
+  > div:nth-child(3) {
+    display: flex;
+    justify-content: space-around;
+  }
+  > div:nth-child(3) img {
+    width: 35%;
+  }
+
+  @media only screen and (min-width: 1700px) {
+    height: 150px;
+
+    > div:nth-child(1) img {
+      width: 200%;
+    }
+    nav {
+      width: 65%;
+      padding: 3% 0% 2% 0%;
+      margin-left: 15%;
+    }
+    nav a {
+      font-size: 25px;
+      margin-right: 1%;
+    }
+    > div:nth-child(3) img {
+      width: 45%;
+    }
+  }
+
+  @media only screen and (min-width: 2900px) {
+    nav a {
+      font-size: 40px;
+    }
+    nav {
+      width: 65%;
+      padding: 3% 0% 2% 0%;
+      margin-left: 15%;
+    }
+    nav {
+      width: 65%;
+      padding: 3% 0% 2% 0%;
+      margin-left: 15%;
+    }
+    > div:nth-child(1) img {
+      width: 450%;
+      margin-left: 25%;
+    }
+  }
+  @media only screen and (min-width: 3500px) {
+    nav a {
+      font-size: 45px;
+    }
+    nav {
+      width: 65%;
+      padding: 3% 0% 2% 0%;
+      margin-left: 15%;
+    }
+    nav {
+      width: 65%;
+      padding: 3% 0% 2% 0%;
+      margin-left: 15%;
+    }
+    > div:nth-child(1) img {
+      width: 450%;
+      margin-left: 25%;
+    }
+  }
+  @media only screen and (min-width: 4000px) {
+    > div:nth-child(1) img {
+      width: 520%;
+    }
+    nav a {
+      font-size: 55px;
+    }
+    > div:nth-child(3) {
+      width: 13%;
+    }
+    > div:nth-child(3) img {
+      width: 65%;
+    }
+  }
+  @media only screen and (max-width: 1700px) {
+    nav {
+      width: 65%;
+      padding: 3% 0% 2% 0%;
+      margin-left: 15%;
+    }
+    nav a {
+      font-size: 20px;
+    }
+    > div:nth-child(1) img {
+      width: 150%;
+      margin-left: 25%;
+    }
+  }
+  @media only screen and (max-width: 1140px) {
+    nav {
+      width: 65%;
+      padding: 3% 0% 2% 0%;
+      margin-left: 15%;
+    }
+    nav a {
+      font-size: 15px;
+    }
+    > div:nth-child(1) img {
+      width: 120%;
+      margin-left: 25%;
+    }
+  }
+  @media only screen and (max-width: 900px) {
+    nav {
+      display: flex;
+      justify-content: space-between;
+      width: 70%;
+      padding: 3% 2% 2% 2%;
+      margin-left: 9%;
+    }
+    nav a {
+      font-size: 10px;
+    }
+    > div:nth-child(1) img {
+      width: 80%;
+    }
+  }
+  @media only screen and (max-width: 752px) {
+    > div:nth-child(1) img {
+      width: 2.5rem;
+      margin-left: 0%;
+    }
+    > div:nth-child(2) img {
+      width: 80%;
+    }
+    > div:nth-child(1) {
+      padding: 3% 3% 3% 5%;
+    }
+    > div:nth-child(2) {
+      width: 32%;
+      padding-left: 5%;
+    }
+  }
+`;
+
+const MobileNav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  padding: 5% 0% 3% 8%;
+  position: fixed;
+  width: 100%;
+  z-index: 999;
+  margin-top: 18%;
+
+  a {
+    text-decoration: none;
+    color: Black;
+    font-family: "M SemiBold";
+    font-size: 18px;
+    margin-bottom: 16px;
+  }
+`;
+
+const Header = () => {
+  const windowSize = useWindowSize();
+  const [nav, setNav] = useState(false);
+  const { languages, changeLanguage } = useI18next();
+  const { t } = useTranslation("header");
+
+  return windowSize > 752 ? (
+    <HeaderTag>
+      <div>
+        <Link to="inicio">
+          <img src={Logo} alt="logo" />
+        </Link>
+      </div>
+      <nav>
+        <Link to="inicio">{t("home")}</Link>
+        <Link to="sobre">{t("about")}</Link>
+        <Link to="servicios">{t("services")}</Link>
+        <Link to="instalaciones">{t("instalations")}</Link>
+        <Link to="cita">{t("appointment")}</Link>
+        <Link to="testimonios">{t("testimonials")}</Link>
+        <Link to="contacto">{t("contact")}</Link>
+      </nav>
+      <SwitchLeng />
+    </HeaderTag>
+  ) : (
+    <div>
+      <HeaderTag>
+        <div>
+          <img src={Menu} nav={nav} onClick={() => setNav(!nav)} />
+        </div>
+        <div>
+          <Link to="inicio">
+            <img src={Logo} alt="logo" />
+          </Link>
+        </div>
+        <div>
+            <SwitchLeng />
+        </div>
+      </HeaderTag>
+      {nav ? (
+        <MobileNav>
+          <Link to="inicio">{t("home")}</Link>
+          <Link to="sobre">{t("us")}</Link>
+          <Link to="servicios">{t("brands")}</Link>
+          <Link to="instalaciones">{t("news")}</Link>
+          <Link to="cita">{t("contact")}</Link>
+        </MobileNav>
+      ) : null}
+    </div>
+  );
+};
+
+export default Header;
