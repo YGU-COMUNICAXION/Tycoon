711d843fe4584d64755fc27ce6c2bcf028d10111
Emanuel Juarez <emanueljuarez.webdev@gmail.com>
2022-12-06 18:27:03 -0600

    mailto corregido

--- src/pages/contactPage.jsx
+++ src/pages/contactPage.jsx
@@ -104,7 +104,7 @@ const ContactPage = () => {
                     <p className='contactP'>+ (506) 4001 7397</p>
                     <p className='contactSubtitle'>{t("contact")}</p>
                     <p className='contactP'>Dinnia Madriz</p>
-                    <a href='mailto:'>
+                    <a href='mailto:dinnia.madriz@tycoon.mx'>
                         <p className='contactP'>dinnia.madriz@tycoon.mx</p>
                     </a>
                 </PaisContacto>
