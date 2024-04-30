8368c57b7714bb252089af674a134b97000a0156
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-04-30 12:35:50 -0600

    fix: spin master translate page

--- src/pages/brands/spinmaster-CAM.jsx
+++ src/pages/brands/spinmaster-CAM.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { graphql } from "gatsby";
 import Layout from "../../components/layout";
 import MarcaPage from "../../components/marcaPage";
 import Banner from "../../images/desktop/marcas/spinmaster/spinmasterBanner.png";
 import Rubik from "../../images/desktop/marcas/spinmaster/rubiks.png";
 import useGeo from "../../hooks/useGeo";
 
-const SonyPage = () => {
+const SpinMaster = () => {
   const geo = useGeo();
 
   return (
@@ -17,4 +18,18 @@ const SonyPage = () => {
   );
 };
 
-export default SonyPage;
+export default SpinMaster;
+
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
--- src/pages/brands/spinmaster.jsx
+++ src/pages/brands/spinmaster.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { graphql } from "gatsby";
 import Layout from "../../components/layout";
 import MarcaPage from "../../components/marcaPage";
 import Banner from "../../images/desktop/marcas/spinmaster/spinmasterBanner.png";
@@ -6,7 +7,7 @@ import Rubik from "../../images/desktop/marcas/spinmaster/rubiks.png";
 import Unicorn from "../../images/desktop/marcas/spinmaster/unicorn.png";
 import useGeo from "../../hooks/useGeo";
 
-const SonyPage = () => {
+const SpinMaster = () => {
   const geo = useGeo();
 
   return (
@@ -19,4 +20,19 @@ const SonyPage = () => {
   );
 };
 
-export default SonyPage;
+export default SpinMaster;
+
+
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
