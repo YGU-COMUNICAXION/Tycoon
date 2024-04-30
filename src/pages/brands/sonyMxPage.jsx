a475f1cddc9ccb42dbd5f62abf6473c9dd363c24
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-04-30 02:39:36 -0600

    add: sony y vizmedia marcas

--- src/pages/brands/sonyMxPage.jsx
+++ src/pages/brands/sonyMxPage.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
-import Layout from '../../components/layout'
-import MarcaPage from '../../components/marcaPage'
-import SonnyBanner from "../../images/desktop/marcas/sony/sonyBanner.webp"
-import Braking from "../../images/desktop/marcas/sony/breakingbad.png"
-import Cobra from "../../images/desktop/marcas/sony/cobrakai.png"
-import Death from "../../images/desktop/marcas/sony/deathnote.png"
-import Ghost from "../../images/desktop/marcas/sony/ghost.png"
-import Boys from "../../images/desktop/marcas/sony/theboys.png"
-import useGeo from '../../hooks/useGeo'
+import React from "react";
+import Layout from "../../components/layout";
+import MarcaPage from "../../components/marcaPage";
+import SonnyBanner from "../../images/desktop/marcas/sony/sonyBanner.webp";
+import Braking from "../../images/desktop/marcas/sony/breakingbad.png";
+import Cobra from "../../images/desktop/marcas/sony/cobrakai.png";
+import Paddington from "../../images/desktop/marcas/sony/paddingto.png";
+import Ghost from "../../images/desktop/marcas/sony/ghost.png";
+import Boys from "../../images/desktop/marcas/sony/theboys.png";
+import Karatekid from "../../images/desktop/marcas/sony/karatekid.png";
+import useGeo from "../../hooks/useGeo";
 
 const SonyPage = () => {
-  const geo = useGeo();  
+  const geo = useGeo();
 
   return (
     <Layout>
       <MarcaPage backgroundImage={SonnyBanner}>
-        <img src={Ghost}/>
-        <img src={Cobra}/>
-        <img src={Braking}/>
-        <img src={Death}/>
+        <img src={Braking} />
+        <img src={Cobra} />
+        <img src={Ghost} />
+        <img src={Paddington} />
+        <img src={Boys} />
+        <img src={Karatekid} />
       </MarcaPage>
     </Layout>
-  )
-}
+  );
+};
 
-export default SonyPage
+export default SonyPage;
--- src/pages/brands/vizmediaMxPage.jsx
+++ src/pages/brands/vizmediaMxPage.jsx
@@ -1,28 +1,46 @@
-import React from 'react'
-import Layout from '../../components/layout'
-import MarcaPage from '../../components/marcaPage'
-import VizBanner from "../../images/desktop/marcas/viz/vizBanner.png"
-import Onepunch from "../../images/desktop/marcas/viz/onepunch.png"
-import Bleach from "../../images/desktop/marcas/viz/bleach.png"
-import NarutoShi from "../../images/desktop/marcas/viz/narutoship.png"
-import Naruto from "../../images/desktop/marcas/viz/naruto.png"
-import Death from "../../images/desktop/marcas/viz/death.png"
-import useGeo from '../../hooks/useGeo'
+import React from "react";
+import Layout from "../../components/layout";
+import MarcaPage from "../../components/marcaPage";
+import VizBanner from "../../images/desktop/marcas/viz/vizBanner.png";
+import Bleach from "../../images/desktop/marcas/viz/bleach.png";
+import Boruto from "../../images/desktop/marcas/viz/boruto.png";
+import capitantsubasa from "../../images/desktop/marcas/viz/capitantsubasa.png";
+import hunter from "../../images/desktop/marcas/viz/hunter.png";
+import inuyasha from "../../images/desktop/marcas/viz/inuyasha.png";
+import jojos from "../../images/desktop/marcas/viz/jojos.png";
+import komi from "../../images/desktop/marcas/viz/komi.png";
+import uzumaki from "../../images/desktop/marcas/viz/uzumaki.png";
+import yashahime from "../../images/desktop/marcas/viz/yashahime.png";
+import zom100 from "../../images/desktop/marcas/viz/zom100.png";
+import Onepunch from "../../images/desktop/marcas/viz/onepunch.png";
+import NarutoShi from "../../images/desktop/marcas/viz/narutoship.png";
+import Naruto from "../../images/desktop/marcas/viz/naruto.png";
+import Death from "../../images/desktop/marcas/viz/death.png";
+import useGeo from "../../hooks/useGeo";
 
 const VizPage = () => {
-  const geo = useGeo();  
+  const geo = useGeo();
 
   return (
     <Layout>
       <MarcaPage backgroundImage={VizBanner}>
-        <img src={Naruto}/>
-        <img src={NarutoShi}/>
-        <img src={Onepunch}/>
-        <img src={Bleach}/>
-        <img src={Death}/>
+        <img src={Bleach} />
+        <img src={Boruto} />
+        <img src={capitantsubasa} />
+        <img src={Death} />
+        <img src={hunter} />
+        <img src={inuyasha} />
+        <img src={jojos} />
+        <img src={komi} />
+        <img src={NarutoShi} />
+        <img src={Naruto} />
+        <img src={Onepunch} />
+        <img src={uzumaki} />
+        <img src={yashahime} />
+        <img src={zom100} />
       </MarcaPage>
     </Layout>
-  )
-}
+  );
+};
 
-export default VizPage
+export default VizPage;
